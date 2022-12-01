8767e165910cd013783a50db31d60751f9a0c3f5
THEDAN320 <103934625+THEDAN320@users.noreply.github.com>
2022-12-01 22:22:44 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,30 +1,77 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-void areth(int* arr) {
+void get_math_wait(double* arr, double &math_wait) {
     double ar = 0;
     int count;
 
     for (count = 0 ; count < 10 ; count++ ) {
         ar += arr[count];
     }
-        cout << ar/count;
+    math_wait = ar/count;
+    cout<<"math wait = "<<math_wait<<endl;
 }
 
-void math_wait(int* arr) {
-    double x = 0, z = 2.2;
-    for (int i = 0; i < 10; i++) {
-        x += arr[i] - z;
+void get_dispersia(double* arr,double math_wait, double &dispersia) {
+    int count;
+    for (count = 0 ; count < 10 ; count++) {
+        dispersia += pow((arr[count] - math_wait), 2);
     }
-    double x 
+    dispersia /= count - 1;
+    cout<<"dispersia = "<<dispersia<<endl;
+    cout<<"otklonenie = "<<pow(dispersia,0.5)<<endl;
 }
 
+void get_normalization(double* arr,double math_wait,double &normalization){
+    int min = arr[0], max =arr[0];
+    for(int i = 1; i < 10; i++){
+        if(min > arr[i]){
+            min = arr[i];
+        }
+        
+        if(max < arr[i]){
+            max = arr[i];
+        }
+    }
+    normalization = (math_wait - min)/(max - min);
+    cout<<"normalization max min = "<<normalization<<endl;
+}
+
+void three_sigm_rule(double math_wait, double dispersia){
+    double choice;
+    double sigma_up = math_wait + 3 * pow(dispersia, 0.5);
+    double sigma_dawn = math_wait - 3 * pow(dispersia, 0.5);
+    cout<<"enter number - ";
+    cin>>choice;
+    
+    if(sigma_up > choice && choice > sigma_dawn){
+        cout<<"this number falls under the 3 sigma rule"<<endl;
+    }
+    else{
+        cout<<"this number does not fall under the 3 sigma rule"<<endl;
+    }
+}
 
 int main()
 {
-    int arr[10] = { 5, 1, 0, 9, 7, 15, 18,-12, -1, -20 };
-
-    areth(arr);
+    double arr[10];
+    for (int count = 0 ; count < 10 ; count++) {
+        double user_input;
+        cout<<"enter number - ";
+        cin>>user_input;
+        arr[count] = user_input;
+    }
+    
+    double math_wait = 0;
+    double dispersia = 0;
+    double normalization = 0;
+    
+    get_math_wait(arr, math_wait);
+    get_dispersia(arr, math_wait, dispersia);
+    get_normalization(arr, math_wait, normalization);
+    
+    while(true){
+        three_sigm_rule(math_wait,dispersia);
+    }
 }
-
-
